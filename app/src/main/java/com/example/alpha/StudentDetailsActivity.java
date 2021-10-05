8500462b34b397ea148fa639f84266549d7822e3
elywin <lukeeraelywin@gmail.com>
2021-10-05 23:44:43 +0300

    fetch data from database

--- app/src/main/java/com/example/alpha/StudentDetailsActivity.java
+++ app/src/main/java/com/example/alpha/StudentDetailsActivity.java
@@ -2,8 +2,12 @@ package com.example.alpha;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.ContentValues;
 import android.content.Intent;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
@@ -13,102 +17,107 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
-
-import java.util.ArrayList;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class StudentDetailsActivity extends AppCompatActivity {
-    DBhelper dBhelper;
-
+    ContentValues val=new ContentValues();
+    ListView list;
+    String arruser[],arrpass[];
+    SQLiteDatabase dBhelper;
+    @SuppressLint("WrongConstant")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_details);
-        dBhelper = new DBhelper(this);
-        Bundle extras = getIntent().getExtras();
-        {
-            int Value = extras.getInt("id");
+        list = (ListView)findViewById(R.id.listView1);
+        dBhelper = openOrCreateDatabase("student.db", SQLiteDatabase.CREATE_IF_NECESSARY,null);
+        Cursor c = dBhelper.query("student",null,null,null,null,null,null);
+        arruser=new String[c.getCount()];
+        arrpass=new String[c.getCount()];
+        c.moveToFirst();
+        for(int i=0;i<arruser.length;i++){
+            arruser[i]=c.getString(0);
+            arrpass[i]=c.getString(1);
 
-            if (Value > 0) {
-                //means this is the view part not the add contact part.
-                Cursor rs = dBhelper.getAllStudents(Value);
-                id_To_Update = Value;
-                rs.moveToFirst();
+        }
 
-                String stuname = rs.getString(rs.getColumnIndex(DBHelper.CONTACTS_COLUMN_STUNAME));
-                String stuphone = rs.getString(rs.getColumnIndex(DBHelper.CONTACTS_COLUMN_STUPHONE));
-                String stuemail = rs.getString(rs.getColumnIndex(DBHelper.CONTACTS_COLUMN_STUEMAIL));
-                String stustreet = rs.getString(rs.getColumnIndex(DBHelper.CONTACTS_COLUMN_STUSTREET));
-                String stuplace = rs.getString(rs.getColumnIndex(DBHelper.CONTACTS_COLUMN_STUCITY));
-
-                if (!rs.isClosed()) {
-                    rs.close();
-                }
-
-                Button backbtn = (Button) findViewById(R.id.backbtn);
-        TextView logout = (TextView) findViewById(R.id.logout);
-        TextView copyright = (TextView) findViewById(R.id.copyright);
-        copyright.setTextColor(Color.BLUE);
-        Button callbtn = (Button) findViewById(R.id.callbtn);
-
-        Intent intent = getIntent();
-        TextView fn = findViewById(R.id.fname2);
-        TextView ln = findViewById(R.id.lnamev);
-        TextView ps = findViewById(R.id.pstudyv);
-        TextView ys = findViewById(R.id.ystdyv);
-        TextView cu = findViewById(R.id.cuv);
-        TextView ad = findViewById(R.id.resiv);
-
-        String fnn = intent.getExtras().getString("Fname");
-        String lnn = intent.getExtras().getString("Lname");
-        String pss = intent.getExtras().getString("pstudy");
-        String cuu = intent.getExtras().getString("cu");
-        String add = intent.getExtras().getString("address");
-        String yss = intent.getExtras().getString("ystudy");
-
-
-        fn.setText(fnn);
-        ln.setText(lnn);
-        ps.setText(pss);
-        ys.setText(yss);
-        cu.setText(cuu);
-        ad.setText(add);
-
-        backbtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), StudentRegistrationActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        logout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent logout = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(logout);
-            }
-        });
-
-        copyright.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent=new Intent(Intent.ACTION_VIEW);
-
-                intent.setData(Uri.parse("https://elywin.github.io/"));
-
-                startActivity(intent);
-            }
-        });
-        callbtn.setOnClickListener(new View.OnClickListener() {
-             @Override
-             public void onClick(View v) {
-                   Intent launch = new Intent(Intent.ACTION_DIAL,Uri.parse("tel:123456798"));
-                   startActivity(launch);
-    }
-});
-
-
+        ArrayAdapter<String>app =new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
+        list.setAdapter(app);
 
     }
 
 }
+//
+//
+//
+//
+//
+//                Button backbtn = (Button) findViewById(R.id.backbtn);
+//        TextView logout = (TextView) findViewById(R.id.logout);
+//        TextView copyright = (TextView) findViewById(R.id.copyright);
+//        copyright.setTextColor(Color.BLUE);
+//        Button callbtn = (Button) findViewById(R.id.callbtn);
+//
+//        Intent intent = getIntent();
+//        TextView fn = findViewById(R.id.fname2);
+//        TextView ln = findViewById(R.id.lnamev);
+//        TextView ps = findViewById(R.id.pstudyv);
+//        TextView ys = findViewById(R.id.ystdyv);
+//        TextView cu = findViewById(R.id.cuv);
+//        TextView ad = findViewById(R.id.resiv);
+//
+//        String fnn = intent.getExtras().getString("Fname");
+//        String lnn = intent.getExtras().getString("Lname");
+//        String pss = intent.getExtras().getString("pstudy");
+//        String cuu = intent.getExtras().getString("cu");
+//        String add = intent.getExtras().getString("address");
+//        String yss = intent.getExtras().getString("ystudy");
+//
+//
+//        fn.setText(fnn);
+//        ln.setText(lnn);
+//        ps.setText(pss);
+//        ys.setText(yss);
+//        cu.setText(cuu);
+//        ad.setText(add);
+//
+//        backbtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(getApplicationContext(), StudentRegistrationActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+//
+//        logout.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent logout = new Intent(getApplicationContext(), MainActivity.class);
+//                startActivity(logout);
+//            }
+//        });
+//
+//        copyright.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent=new Intent(Intent.ACTION_VIEW);
+//
+//                intent.setData(Uri.parse("https://elywin.github.io/"));
+//
+//                startActivity(intent);
+//            }
+//        });
+//        callbtn.setOnClickListener(new View.OnClickListener() {
+//             @Override
+//             public void onClick(View v) {
+//                   Intent launch = new Intent(Intent.ACTION_DIAL,Uri.parse("tel:123456798"));
+//                   startActivity(launch);
+//    }
+//});
+
+
+
+//    }
+//
+//}
